1c17678372ddbcce1a6bb158bbb2c0086b7842d4
Ma28hew <98984621+Ma28hew@users.noreply.github.com>
2022-02-11 12:58:26 -0700

    Add files via upload

---
+++ Util.js
@@ -0,0 +1,9 @@
+// RGB Colors with JS by Mr. M
+
+// Event Listener
+document.getElementById("preview-btn").addEventListener("click", rgbPreview);
+
+// Event Function
+function rgbPreview() {
+    alert("rgbPreview");
+}
