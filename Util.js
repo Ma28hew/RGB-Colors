846af0fd48b033b3bd7ca58cc30e07de1bade37c
Ma28hew <98984621+Ma28hew@users.noreply.github.com>
2022-02-11 12:57:26 -0700

    Delete Util.js

--- Util.js
+++
@@ -1,9 +0,0 @@
-// Landing Page
-
-// Prompt User for Data
-let username = prompt("What is your name?")
-let focus1 = prompt("What is your main focus for today")
-
-// Output Data into the page
-document.getElementById("nameSpan").innerHTML = username;
-document.getElementById("focusP").innerHTML = focus1;
