93be045af0aaeb44e55638c66c3c344547a86a6f
Ma28hew <98984621+Ma28hew@users.noreply.github.com>
2022-02-24 12:52:55 -0700

    Add files via upload

--- Util.js
+++ Util.js
@@ -5,5 +5,16 @@ document.getElementById("preview-btn").addEventListener("click", rgbPreview);
 
 // Event Function
 function rgbPreview() {
-    alert("rgbPreview");
+    
+// Input
+let rValue = document.getElementById("redIn").value;
+let gValue = document.getElementById("greenIn").value;
+let bValue = document.getElementById("blueIn").value;
+   
+// Processing
+    let rgbString = "rgb(" + rValue + ", " + gValue + ", " + bValue + ")";
+    
+    // Output
+document.getElementById("rgbStrOut").innerHTML = rgbString;
+document.getElementById("display").style.background = rgbString;
 }
